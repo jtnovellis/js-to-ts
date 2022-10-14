248d7315d7b6e5f35901808c8cc44c3835e0bc26
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-10-14 10:23:34 -0500

    final commit

--- index.ts
+++ index.ts
@@ -10,19 +10,25 @@ function open(number: string, balance: number): void {
 }
 
 function getBalance(number: string): number {
-  const account = accounts.find((account) => account.number === number);
+  const account: Account | undefined = accounts.find(
+    (account) => account.number === number
+  );
   if (!account) throw new Error('La cuenta no se encontró');
   return account.balance;
 }
 
 function deposit(number: string, amount: number): void {
-  const account = accounts.find((account) => account.number === number);
+  const account: Account | undefined = accounts.find(
+    (account) => account.number === number
+  );
   if (!account) throw new Error('La cuenta no se encontró');
   account.balance += amount;
 }
 
 function withdraw(number: string, amount: number): void {
-  const account = accounts.find((account) => account.number === number);
+  const account: Account | undefined = accounts.find(
+    (account) => account.number === number
+  );
   if (!account) throw new Error('La cuenta no se encontró');
   if (account.balance - amount < 0) throw new Error('Fondos insuficientes');
   account.balance -= amount;
